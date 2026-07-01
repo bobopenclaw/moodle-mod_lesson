a6ea1a1a70ef280f93bd9edc7ace1ca456993646
Matthias Opitz <opitz@gmx.de>
2026-07-01 09:46:21 +0100

    MDACH26 Add appearance thumbnail selector

--- classes/local/controller.php
+++ classes/local/controller.php
@@ -43,6 +43,75 @@ class controller {
     }
 
     /**
+     * Get available appearance designs with display metadata for tile selectors.
+     *
+     * @return array List of design metadata objects.
+     */
+    public static function get_appearance_design_tiles(): array {
+        global $DB;
+
+        $tiles = [
+            (object) [
+                'value' => '',
+                'name' => get_string('none'),
+                'type' => '',
+                'typename' => get_string('default'),
+                'previewurl' => '',
+            ],
+        ];
+
+        $designs = $DB->get_records('lesson_appearance_designs', ['enabled' => 1], 'name ASC', 'id, uniqueid, name, type');
+        foreach ($designs as $design) {
+            $component = 'lessonappearance_' . $design->type;
+            $typename = get_string_manager()->string_exists('pluginname', $component)
+                ? get_string('pluginname', $component)
+                : $design->type;
+
+            $tiles[] = (object) [
+                'value' => $design->uniqueid,
+                'name' => format_string($design->name),
+                'type' => $design->type,
+                'typename' => $typename,
+                'previewurl' => self::get_appearance_design_preview_url($design),
+            ];
+        }
+
+        return $tiles;
+    }
+
+    /**
+     * Get a preview image URL for an appearance design when its subplugin provides one.
+     *
+     * @param \stdClass $design Appearance design record.
+     * @return string
+     */
+    private static function get_appearance_design_preview_url(\stdClass $design): string {
+        $context = \context_system::instance();
+        $component = 'lessonappearance_' . $design->type;
+        $filearea = 'appearance_preview';
+
+        $fs = get_file_storage();
+        $files = $fs->get_area_files($context->id, $component, $filearea, $design->id, 'sortorder', false);
+        if (!$files) {
+            return '';
+        }
+
+        $file = reset($files);
+        if (!$file) {
+            return '';
+        }
+
+        return \moodle_url::make_pluginfile_url(
+            $context->id,
+            $component,
+            $filearea,
+            $design->id,
+            $file->get_filepath(),
+            $file->get_filename()
+        )->out(false);
+    }
+
+    /**
      * Resolve the appearance subplugin instance for a given lesson.
      *
      * Looks up the lesson's selected design (by uniqueid), loads the
